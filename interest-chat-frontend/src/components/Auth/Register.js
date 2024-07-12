e2698e913bf812d5e579f36c791360eb60c3f0d3
Rudransh Arora <rudransh1482@gmail.com>
2024-07-12 13:39:07 +0530

    Created a react app and decided a structure for it. Added Login and Register feature in auth component

---
+++ interest-chat-frontend/src/components/Auth/Register.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../../slices/authSlice';
+
+const Register = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(registerUser({ username, email, password }));
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Register</button>
+        </form>
+    );
+};
+
+export default Register;
